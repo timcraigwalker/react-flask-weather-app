955d22047b9fddd1317d0be67091b96eb9730c0d
Timothy Walker <timwalker1994@gmail.com>
2024-02-26 16:49:15 +0000

    moved external apis to separate view

--- api/app.py
+++ api/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_login import login_required
-import requests
 
 from extensions import api, bcrypt, db, login_manager, migrate
-from models import User, UserFavouriteCity
-from views import user_blp, user_favourite_city_blp
+from models import User
+from views import api_blp, user_blp, user_favourite_city_blp
 
 app = Flask("react-flask-weather-app")
 app.config.from_object("config")
@@ -22,6 +20,7 @@ login_manager.init_app(app)
 with app.app_context():
     db.create_all()
 
+api.register_blueprint(api_blp, path="api")
 api.register_blueprint(user_blp, path="user")
 api.register_blueprint(
     user_favourite_city_blp,
@@ -32,37 +31,3 @@ api.register_blueprint(
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
-
-@app.route("/api/cities/", methods=["GET"], defaults={"name_prefix": ""})
-@app.route("/api/cities/<name_prefix>", methods=["GET"])
-@login_required
-def cities(name_prefix):
-    url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
-
-    params = {"maxPopulation": "1000000", "namePrefix": name_prefix}
-
-    headers = {
-        "X-RapidAPI-Key": app.config["RAPID_API_KEY"],
-        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-
-    return response.json()
-
-
-@app.route("/api/weather/<latitude>/<longitude>", methods=["GET"])
-@login_required
-def weather(latitude, longitude):
-    url = "https://api.openweathermap.org/data/2.5/weather"
-
-    params = {
-        "appid": app.config["OPENWEATHER_API_KEY"],
-        "lat": latitude,
-        "lon": longitude
-    }
-
-    response = requests.get(url, params=params)
-
-    return response.json()
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,7 +1,9 @@
+from views.api import api_blp
 from views.user import user_blp
 from views.user_favourite_city import user_favourite_city_blp
 
 __all__ = [
+    "api_blp",
     "user_blp",
     "user_favourite_city_blp"
 ]
