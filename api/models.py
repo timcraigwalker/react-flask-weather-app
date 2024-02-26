3450f920e844a649c6e0ebae8e6f5c5f2e359c97
Timothy Walker <timwalker1994@gmail.com>
2024-02-26 10:14:06 +0000

    added user favourite city table

--- api/models.py
+++ api/models.py
@@ -17,3 +17,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=create_uuid)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.Text, nullable=False)
+
+    favourite_cities = db.relationship(
+        "FavouriteCity", back_populates="user", uselist=True
+    )
+
+
+class UserFavouriteCity(db.Model):
+    __tablename__ = "user_favourite_cities"
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=create_uuid)
+    user_id = db.Column(
+        db.String(32), db.ForeignKey("users.id"), nullable=False
+    )
+    city = db.Column(db.String(150), nullable=False)
+    latitude = db.Column(db.Float(precision=10, asdecimal=False))
+    longitide = db.Column(db.Float(precision=10, asdecimal=False))
+
+    user = db.relationship(
+        "Users", back_populates="favourite_cities"
+    )
