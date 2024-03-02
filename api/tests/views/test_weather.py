703e9032d106a2893ef95f53a8144e0ba1e25407
Timothy Walker <timwalker1994@gmail.com>
2024-03-02 19:08:46 +0000

    updated weather view to include forecast endpoint

--- api/tests/views/test_weather.py
+++ api/tests/views/test_weather.py
@@ -2,10 +2,20 @@ import mock
 
 
 @mock.patch("views.weather.requests.get")
-def test_weather(app, client, db, user_favourite_city):
+def test_weather_current(app, client, db, user_favourite_city):
     lat = user_favourite_city.latitude
     long = user_favourite_city.longitude
 
-    response = client.get(f"api/weather/{lat}/{long}")
+    response = client.get(f"api/weather/current/{lat}/{long}")
+
+    assert response.status_code == 200
+
+
+@mock.patch("views.weather.requests.get")
+def test_weather_forecast(app, client, db, user_favourite_city):
+    lat = user_favourite_city.latitude
+    long = user_favourite_city.longitude
+
+    response = client.get(f"api/weather/forecast/{lat}/{long}")
 
     assert response.status_code == 200
--- api/views/weather.py
+++ api/views/weather.py
@@ -12,13 +12,33 @@ weather_blp = Blueprint(
 
 
 @weather_blp.route(
-    "/<latitude>/<longitude>",
+    "current/<latitude>/<longitude>",
     methods=["GET"],
     tags=["weather"]
 )
 @login_required
 def weather(latitude, longitude):
     url = "https://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+        "appid": current_app.config["OPENWEATHER_API_KEY"],
+        "lat": latitude,
+        "lon": longitude
+    }
+
+    response = requests.get(url, params=params)
+
+    return response.json()
+
+
+@weather_blp.route(
+    "forecast/<latitude>/<longitude>",
+    methods=["GET"],
+    tags=["weather"]
+)
+@login_required
+def forecast(latitude, longitude):
+    url = "https://api.openweathermap.org/data/2.5/forecast"
 
     params = {
         "appid": current_app.config["OPENWEATHER_API_KEY"],
