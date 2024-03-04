916a1ee08eda31d0d4f4ac2e5451134c2ba5d1b9
Timothy Walker <timwalker1994@gmail.com>
2024-03-04 12:09:12 +0000

    updated response json variable

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -12,9 +12,9 @@ const Search = ({onSearchChange}) => {
     const getCities = async (inputValue) => {
         try {
             const response = await fetch(`api/cities/${inputValue}`);
-            const response_1 = await response.json();
+            const responseJson = await response.json();
             return {
-                options: response_1.data.map((city) => {
+                options: responseJson.data.map((city) => {
                     return {
                         value: `${city.latitude} ${city.longitude}`,
                         label: `${city.name}, ${city.countryCode}`,
