3bf9c82167c7c26df8a7e89222d4aff2e5e72fa9
Timothy Walker <timwalker1994@gmail.com>
2024-03-05 10:53:43 +0000

    updated header to use mat ui

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,21 +1,68 @@
-import "./header.css"
+import { AccountCircle } from "@mui/icons-material";
+import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-const Header = () => {
-    const logout = () => {
+const Header = ({user}) => {
+    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleLogout = async () => {
         const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
+            method: "POST",
+            headers: { "Content-Type": "application/json" }
         };
-        return fetch('/auth/logout', requestOptions)
+        const response = await fetch("api/auth/logout", requestOptions);
+        if(response.status === 200) navigate("/login");
     }
 
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div className="header">
-            <div className="title">Weather App</div>
-            <div className="account-icon">
-                <button onClick={logout}>Logout</button>
-            </div>
-        </div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Weather App
+                </Typography>
+                {user && (
+                    <div>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                    >
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                    </div>
+                )}
+            </Toolbar>
+        </AppBar>
     )
 }
 
