332c032f803522d6c54c24828236025dda384664
Timothy Walker <timwalker1994@gmail.com>
2024-03-05 11:28:42 +0000

    updated paths to pass user prop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import './App.css';
-import React from 'react';
+import React, { createContext, useEffect, useState } from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
   Route,
+  BrowserRouter as Router,
+  Routes
 } from "react-router-dom";
+import "./App.css";
 
-import Header from './components/Header/header'
-import Login from './pages/login';
-import Home from './pages/home';
+import Header from "./components/Header/header";
+import PrivateRoute from "./components/PrivateRoute/privateRoute";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+const AuthContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("api/user")
+    .then(async (response) => {
+        const userResponse = await response.json();
+        if(response.status === 200)
+        {
+          setUser(userResponse);
+        }
+    })
+    .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header user={user}/>
+        <div className="container">
+          <Routes>
+            <Route path="/test" element={<PrivateRoute user={user} />}>
+              <Route path="/test" element={<Home user={user} />} />
+            </Route>
+            
+            <Route exact path="/" element={<Home user={user} />} />
+            <Route path="/login" element={<Login user={user} />} />
+            <Route path="/register" element={<Register user={user} />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
